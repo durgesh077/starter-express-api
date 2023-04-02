0ef4976cf79d7d2b67a9e1f771b62bdc9c098e14
durgesh077 <durgeshmaurya07@gmail.com>
2023-04-02 13:33:23 +0530

    added

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import app from './dist/index.js';
+import logger from './dist/src/logger/index.js';
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    logger.info(`Server started: http://localhost:${port}`);
+});
